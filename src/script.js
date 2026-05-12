6ecc501f7ecd85580b1a41d1794d3d8515527b41
Ilodigwe Nzube <ilodigwenzube142@gmail.com>
2026-05-12 16:38:50 +0100

    Module entry point. Added button event listeners.

--- src/script.js
+++ src/script.js
@@ -1,2 +1,12 @@
 import "./style.css";
+import homePage from "./home.js";
+import addTodoPage from "./addTodo.js";
+
+homePage();
+
+const homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", homePage);
 console.log("Hello, World!");
+
+const addTodoButton = document.getElementById("add-todo");
+addTodoButton.addEventListener("click", addTodoPage);
