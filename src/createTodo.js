35bc604bd9396c88556ae36cc682087b3fc11aab
Ilodigwe Nzube <ilodigwenzube142@gmail.com>
2026-05-13 20:39:07 +0100

    Module to create todo

---
+++ src/createTodo.js
@@ -0,0 +1,14 @@
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+  }
+
+  // createTodo() {
+  // const todoTile = document.createElement("div");
+  // todoTile.classList.add("todo-tile");
+
+  // }
+}
