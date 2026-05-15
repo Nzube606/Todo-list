1ad741c40738dda607dc137f3e227e7ed65aad7d
Ilodigwe Nzube <ilodigwenzube142@gmail.com>
2026-05-15 15:29:19 +0100

    Module to handle editing and deletion of a project

---
+++ src/modifyProject.js
@@ -0,0 +1,98 @@
+import projectPage from "./projectPage.js";
+export function editProject(project) {
+  const content = document.querySelector("#content");
+  const dialog = document.createElement("dialog");
+  dialog.classList.add("edit-project-dialog");
+
+  const form = document.createElement("form");
+  form.method = "dialog";
+  dialog.appendChild(form);
+
+  const titleLabel = document.createElement("label");
+  titleLabel.textContent = "Project Title:";
+  titleLabel.htmlFor = "edit-project-title";
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.id = "edit-project-title";
+  titleInput.required = true;
+  titleInput.name = "title";
+  titleInput.placeholder = "Enter new project title";
+
+  form.appendChild(titleLabel);
+  form.appendChild(titleInput);
+
+  const editProjectDialogButtons = document.createElement("div");
+  editProjectDialogButtons.id = "edit-project-dialog-buttons";
+  form.appendChild(editProjectDialogButtons);
+
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.type = "submit";
+  saveButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (titleInput.value.trim() === "") {
+      alert("Please fill in a valid title");
+      return;
+    } else {
+      project.title = titleInput.value;
+      dialog.close();
+      projectPage();
+    }
+  });
+  editProjectDialogButtons.appendChild(saveButton);
+
+  const cancelButton = document.createElement("button");
+  cancelButton.textContent = "Cancel";
+  cancelButton.type = "button";
+  cancelButton.addEventListener("click", () => {
+    dialog.close();
+  });
+  editProjectDialogButtons.appendChild(cancelButton);
+  content.appendChild(dialog);
+  dialog.showModal();
+}
+
+export function deleteProject(projects, current) {
+  const content = document.querySelector("#content");
+  const dialog = document.createElement("dialog");
+  content.appendChild(dialog);
+
+  const form = document.createElement("form");
+  const deleteQuery = document.createElement("p");
+  deleteQuery.id = "delete-query";
+  deleteQuery.textContent = " Are you sure you want to delete this project?";
+  form.appendChild(deleteQuery);
+  dialog.appendChild(form);
+
+  const confirmButtonsDiv = document.createElement("div");
+  confirmButtonsDiv.id = "confirm-buttons-div";
+  form.appendChild(confirmButtonsDiv);
+
+  const yesButton = document.createElement("button");
+  yesButton.textContent = "Yes";
+  yesButton.type = "submit";
+  confirmButtonsDiv.appendChild(yesButton);
+  yesButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const index = projects.findIndex(
+      (project) => project.title === current.title,
+    );
+
+    if (index !== -1) {
+      projects.splice(index, 1);
+      dialog.close();
+      projectPage();
+    }
+  });
+
+  const noButton = document.createElement("button");
+  noButton.textContent = "No";
+  noButton.type = "button";
+  confirmButtonsDiv.appendChild(noButton);
+
+  noButton.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  dialog.showModal();
+}
