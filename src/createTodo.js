bf66dddb9116af9dd278bfa2b70ac42a82752c53
Ilodigwe Nzube <ilodigwenzube142@gmail.com>
2026-05-15 15:27:05 +0100

    Added one parameter to handle the state of the checkbox. Added the getter and setter for the checkbox

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,14 +1,18 @@
-class Todo {
-  constructor(title, description, dueDate, priority) {
+export class Todo {
+  constructor(title, description, dueDate, priority, project, checked) {
     this.title = title;
     this.dueDate = dueDate;
     this.description = description;
     this.priority = priority;
+    this.project = project;
+    this.checked = checked;
   }
 
-  // createTodo() {
-  // const todoTile = document.createElement("div");
-  // todoTile.classList.add("todo-tile");
+  setChecked(checked) {
+    checked === true ? (this.checked = true) : (this.checked = false);
+  }
 
-  // }
+  getChecked() {
+    return this.checked;
+  }
 }
